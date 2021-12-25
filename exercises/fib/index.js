080c4df2c033b91a6a440736b0bb735a31df90b0
ImrulKay3s <kayesimrul224@gmail.com>
2021-12-25 13:57:17 +0600

    fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,7 +7,45 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+function memorize(fn){
+    const cache = {};
+    return function(...args){
+        if(cache[args]){
+            return cache[args];
+        }
 
-function fib(n) {}
+        const result = fn.apply(this,args);
+        cache[args] = result;
+        return result;
+    };
+}
+function slowFib(n) {
+    if(n<2){
+        return n;
+    }
+
+    return slowFib(n-1)+slowFib(n-2);
+}
+
+const fib = memorize(slowFib);
 
 module.exports = fib;
+
+
+// function fib(n) {
+//     if(n<2){
+//         return n;
+//     }
+
+//     return fib(n-1)+fib(n-2);
+// }
+
+// function fib(n) {
+//     const result = [0,1];
+//     for(let i =2;i<=n;i++){
+//         const a = result[i-1];
+//         const b = result[i-2];
+//         result.push(a+b);
+//     }
+//     return result[n];
+// }
